97b5d56dff36e682c8401bacab4836edf3d268d7
Abdullah Tahir <tahirabdullah801@gmail.comm>
2023-03-19 09:43:18 +0500

    working with the errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,13 @@ import { store } from "@/Helpers/redux/store";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import Home from "./index";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <ToastContainer />
     </Provider>
   );
 }
